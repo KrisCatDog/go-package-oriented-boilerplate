298e080e0ed79e7aa48f0567365e016a3d951018
Krishna <ramain77@gmail.com>
2021-09-30 17:45:37 +0700

    feat: add cobra root command & move http handler

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Run: func(cmd *cobra.Command, args []string) {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			_, err := w.Write([]byte("index page"))
+			if err != nil {
+				log.Panicf("error while writing response: %v", err)
+			}
+		})
+
+		log.Fatalln(http.ListenAndServe("1234", nil))
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalln(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
+import "github.com/KrisCatDog/go-package-oriented-boilerplate/cmd"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("hello world"))
-		if err != nil {
-			log.Panicln(err.Error())
-		}
-	})
-
-	log.Fatalln(http.ListenAndServe(":1234", nil))
+	cmd.Execute()
 }
